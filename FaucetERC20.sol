bab8e7547e42fe64567e656dafefe2a368a68ec7
Matlemad <35381854+Matlemad@users.noreply.github.com>
2022-10-24 21:45:40 +0200

    Update FaucetERC20.sol

--- FaucetERC20.sol
+++ FaucetERC20.sol
@@ -1,3 +1,7 @@
+// © by ddbit - https://github.com/ddbit
+// this simple ERC20 faucet allows any msg.sender to receive a preset amount of token 
+// via sending a 0 ETH transaction to the faucet's contract address
+
 pragma solidity ^0.8.7;
 
 interface IERC20 {
